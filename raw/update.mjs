ce5c1e2d5fe0687e64245c26cdd52ac4d08a11ff
Demo Macro <abc@imst.xyz>
2023-12-01 13:31:53 +0800

    feat: add update script to fetch google fonts' data

---
+++ raw/update.mjs
@@ -0,0 +1,28 @@
+import { writeFileSync } from "fs";
+import { Googlefonts } from "@librefont/googlefonts";
+
+const googlefonts = new Googlefonts(process.env.GOOGLE_FONTS_KEY);
+
+const sorts = [null, "alpha", "date", "popularity", "style", "trending"];
+
+const capabilitys = [null, "WOFF2", "VF"];
+
+for (const sort of sorts) {
+  for (const capability of capabilitys) {
+    const params = Object.fromEntries(
+      Object.entries({
+        sort,
+        capability,
+      }).filter(([_, v]) => v !== null),
+    );
+
+    const webfontList = await googlefonts.getWebfonts({
+      ...params,
+    });
+
+    writeFileSync(
+      `./sort_${sort || "default"}_capability_${capability || "default"}.json`,
+      JSON.stringify(webfontList, null, 2),
+    );
+  }
+}
